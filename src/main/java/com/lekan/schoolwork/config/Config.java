1f17d3ae8193e861af540f41021aa046abf0d28e
Olalekan Amoo <olalekanamoo128@gmail.com>
2024-12-17 17:51:59 +0100

    Use one of either the CorsFilter or Config

---
+++ src/main/java/com/lekan/schoolwork/config/Config.java
@@ -0,0 +1,24 @@
+package com.lekan.schoolwork.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class Config {
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter());
+        bean.setOrder(Integer.MIN_VALUE);
+        return bean;
+    }
+}
