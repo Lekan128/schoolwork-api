86198f954d7f6062412d885b01d13a996b584097
Olalekan Amoo <olalekanamoo128@gmail.com>
2024-12-17 16:58:10 +0100

    Edit Web config cors mapping

--- src/main/java/com/lekan/schoolwork/config/WebConfig.java
+++ src/main/java/com/lekan/schoolwork/config/WebConfig.java
@@ -11,8 +11,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://coursereview.netlify.app")
+                .allowedOrigins("*")
                 .allowedMethods("*")
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
